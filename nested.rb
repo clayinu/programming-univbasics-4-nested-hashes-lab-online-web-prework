e6def8d20c128228883427118954444aa381c902
Hoko Le <hokole527@gmail.com>
2019-10-01 18:25:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -72,8 +72,7 @@ def adding_matz
 	programmer_hash = 
  		{
  		    :yukihiro_matsumoto => {
- 		      :known_for => ,
- 		      :languages => []
+
  		    },
         :grace_hopper => {
           :known_for => "COBOL",
