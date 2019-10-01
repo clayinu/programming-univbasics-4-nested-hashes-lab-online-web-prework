1d9e52111e9c15a6244e01e6381231020dd80636
Hoko Le <hokole527@gmail.com>
2019-10-01 18:36:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,7 +87,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto].merge!( {:known_for => "Ruby", :languages => ["LISP", "C"]})
+programmer_hash[:yukihiro_matsumoto].merge!( {:known_for => "Ruby"})
 }
 
 puts programmer_hash
